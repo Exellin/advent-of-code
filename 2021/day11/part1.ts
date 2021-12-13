e13d261d541fb64593953a5736b85290a959bd7d
Shawn Carnegie <carnegieshawn@gmail.com>
2021-12-12 20:06:06 -0700

    Add solution to 2021 day 11

---
+++ 2021/day11/part1.ts
@@ -0,0 +1,50 @@
+import fs from 'fs';
+
+type Octopus = {
+  energy: number,
+  flashed?: boolean,
+  x: number,
+  y: number
+}
+
+const data = fs.readFileSync('day11/input.txt').toString('utf-8').split('\n').filter((line) => line.length > 0);
+const octopi: Octopus[] = [];
+const STEPS = 100;
+const MAX_ENERGY = 9;
+let flashes = 0;
+
+for (let i=0; i<data.length; i++) {
+  const row = Array.from(data[i]).map((value) => Number(value));
+
+  for (let j=0; j<data[i].length; j++) {
+    octopi.push({
+      energy: row[j],
+      y: i,
+      x: j,
+      flashed: false
+    });
+  }
+}
+
+for (let i=1; i<=STEPS; i++) {
+  octopi.forEach((octopus) => octopus.energy++);
+
+  while (octopi.filter((octopus) => octopus.energy > MAX_ENERGY && octopus.flashed === false).length > 0) {
+    octopi.filter((octopus) => octopus.energy > MAX_ENERGY && octopus.flashed === false).forEach((flashingOctopus) => {
+      const surroundingOctopi = octopi.filter((octopus) => {
+        return (Math.abs(flashingOctopus.x - octopus.x) <=1) && (Math.abs(flashingOctopus.y - octopus.y) <=1);
+      });
+
+      surroundingOctopi.forEach((octopus) => octopus.energy++);
+      flashingOctopus.flashed = true;
+      flashes++;
+    });
+  }
+
+  octopi.filter((octopus) => octopus.flashed).forEach((flashedOctopus) => {
+    flashedOctopus.energy = 0;
+    flashedOctopus.flashed = false;
+  });
+}
+
+console.log(`There were a total of ${flashes} flashes across ${STEPS} steps`);
---
+++ 2021/day11/part2.ts
@@ -0,0 +1,57 @@
+import fs from 'fs';
+
+type Octopus = {
+  energy: number,
+  flashed?: boolean,
+  x: number,
+  y: number
+}
+
+const data = fs.readFileSync('day11/input.txt').toString('utf-8').split('\n').filter((line) => line.length > 0);
+const octopi: Octopus[] = [];
+const MAX_ENERGY = 9;
+let allFlashed = false;
+let step = 1;
+
+for (let i=0; i<data.length; i++) {
+  const row = Array.from(data[i]).map((value) => Number(value));
+
+  for (let j=0; j<data[i].length; j++) {
+    octopi.push({
+      energy: row[j],
+      y: i,
+      x: j,
+      flashed: false
+    });
+  }
+}
+
+while (allFlashed === false) {
+  let flashesDuringStep = 0;
+  octopi.forEach((octopus) => octopus.energy++);
+
+  while (octopi.filter((octopus) => octopus.energy > MAX_ENERGY && octopus.flashed === false).length > 0) {
+    octopi.filter((octopus) => octopus.energy > MAX_ENERGY && octopus.flashed === false).forEach((flashingOctopus) => {
+      const surroundingOctopi = octopi.filter((octopus) => {
+        return (Math.abs(flashingOctopus.x - octopus.x) <=1) && (Math.abs(flashingOctopus.y - octopus.y) <=1);
+      });
+
+      surroundingOctopi.forEach((octopus) => octopus.energy++);
+      flashingOctopus.flashed = true;
+      flashesDuringStep++;
+    });
+  }
+
+  octopi.filter((octopus) => octopus.flashed).forEach((flashedOctopus) => {
+    flashedOctopus.energy = 0;
+    flashedOctopus.flashed = false;
+  });
+
+  if (flashesDuringStep === octopi.length) {
+    allFlashed = true;
+  }
+
+  step++;
+}
+
+console.log(`The the step that all octopi flashed at once was step ${step - 1}`);
